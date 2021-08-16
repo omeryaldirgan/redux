799c7c10375fea473e25e412d313b7ffd6f13980
omeryaldirgan <omeryaldirgan@hotmail.com>
2021-08-16 23:02:16 +0300

    Store ve Todos Slice'ı Oluşturuldu.

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,28 +1,23 @@
 import  React from 'react'
+import {useSelector} from "react-redux";
+
 export default function TodoList(){
+   const items=useSelector((state)=>state.todos.items)
+   console.log(items)
    return(
       <ul className="todo-list">
-         <li className="completed">
-            <div className="view">
-               <input className="toggle" type="checkbox"/>
-               <label>Learn JavaScript</label>
-               <button className="destroy"></button>
-            </div>
-         </li>
-         <li>
-            <div className="view">
-               <input className="toggle" type="checkbox"/>
-               <label>Learn React</label>
-               <button className="destroy"></button>
-            </div>
-         </li>
-         <li>
-            <div className="view">
-               <input className="toggle" type="checkbox"/>
-               <label>Have a life!</label>
-               <button className="destroy"></button>
-            </div>
-         </li>
+         {
+            items.map((item,inx)=>(
+               <li key={inx} className={`${item.completed && 'completed'}`}>
+                  <div className="view">
+                     <input className="toggle" type="checkbox"/>
+                     <label>{item.title}</label>
+                     <button className="destroy"></button>
+                  </div>
+               </li>
+            ))
+         }
+
       </ul>
    )
 }
